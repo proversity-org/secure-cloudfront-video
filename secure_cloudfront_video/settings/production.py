b1a9aa998628dbfc324e49d15418f7d13be0bbba
Squirrel18 <daniel.quiroga@edunext.co>
2019-11-18 16:58:18 -0500

    Adding secure-cloudfront-video endpoint.

--- secure_cloudfront_video/settings/production.py
+++ secure_cloudfront_video/settings/production.py
@@ -10,4 +10,15 @@ def plugin_settings(settings):
     Set of plugin settings used by the Open Edx platform.
     More info: https://github.com/edx/edx-platform/blob/master/openedx/core/djangoapps/plugins/README.rst
     """
-    pass
+    settings.SCV_CLOUDFRONT_ID = getattr(settings, 'ENV_TOKENS', {}).get(
+        'SCV_CLOUDFRONT_ID',
+        settings.SCV_CLOUDFRONT_ID,
+    )
+    settings.SCV_CLOUDFRONT_URL = getattr(settings, 'ENV_TOKENS', {}).get(
+        'SCV_CLOUDFRONT_URL',
+        settings.SCV_CLOUDFRONT_URL,
+    )
+    settings.SCV_CLOUDFRONT_PRIVATE_SIGNING_KEY = getattr(settings, 'AUTH_TOKENS', {}).get(
+        'SCV_CLOUDFRONT_PRIVATE_SIGNING_KEY',
+        settings.SCV_CLOUDFRONT_PRIVATE_SIGNING_KEY,
+    )
